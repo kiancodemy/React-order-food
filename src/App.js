4192b2570d4e0e435ed87cd15b7624c7ef908078
kian kheirani <kiankheirani@gmail.com>
2023-07-20 12:06:05 +0430

    'add-final-cart'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Header from "./component/layout/header";
 import Meals from "./component/Meals/Meals";
+import Card from "./component/Cart/Cart";
+import { useState } from "react";
 import { Fragment } from "react";
 function App() {
+  const [cardshow, setcardshow] = useState(false);
+  const showcard = () => {
+    setcardshow(true);
+  };
+  const hidecard = () => {
+    setcardshow(false);
+  };
+
   return (
     <Fragment>
+      <Card></Card>
       <Header></Header>
       <Meals></Meals>
     </Fragment>
