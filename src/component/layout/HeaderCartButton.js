c802ee0752b5795d2b616f890802288e204b967f
kian kheirani <kiankheirani@gmail.com>
2023-07-27 17:18:53 +0430

    'finished'

--- src/component/layout/HeaderCartButton.js
+++ src/component/layout/HeaderCartButton.js
@@ -1,13 +1,25 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import { useState, useEffect } from "react";
+import { Book } from "../../store/context";
+import { useContext } from "react";
 
 const HeaderCartButton = (props) => {
+  const [buttonn, setbutton] = useState(false);
+  const btnclass = `${classes.button} ${buttonn ? classes.bump : " "}`;
+  const { items } = useContext(Book);
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setbutton(true);
+  }, [items]);
   return (
     <button
       onClick={() => {
         props.onshow();
       }}
-      className={classes.button}
+      className={btnclass}
     >
       <span className={classes.icon}>
         <CartIcon />
